78b716d38c6cf111867009b9d1096adb67b680ec
Eld3rr <66041715+Eld3rr@users.noreply.github.com>
2020-07-26 11:53:00 -0300

    Update botinfo.js

--- comandos/informação/botinfo.js
+++ comandos/informação/botinfo.js
@@ -24,23 +24,23 @@ exports.run = async(bot, message, args) => {
     const but = new Discord.MessageEmbed()
     .setAuthor("Sobre Mim")
     .setDescription("Nasci no dia 06/07/2020, em meio a Pandemia do Covid-19!\nMeu intuito Ã© trazer entretenimento e moderaÃ§Ã£o para seu servidor!")
-    .addField("Meu criador:", "Pedr1n â™ª#4537")
-    .addField("Estatisticas", `ðŸ‘¥ Membros: \`${memberCount}\`
-    ðŸ’¾ Servidores: \`${bot.guilds.cache.size}\`
-    ðŸ“ Canais: \`${bot.channels.cache.size}\`
-    ðŸ“Œ Comandos: \`${bot.commands.size}\``)
-    .addField("InformaÃ§Ãµes", `Host: [Heroku](https://heroku.com)
+    .addField("Meu criador:", "Éld3rr_#2583")
+    .addField("Estatisticas", `Membros: \`${memberCount}\`
+    Servidores: \`${bot.guilds.cache.size}\`
+    Canais: \`${bot.channels.cache.size}\`
+    Comandos: \`${bot.commands.size}\``)
+    .addField("Informações", `Host: [Heroku](https://heroku.com)
     Database: [Firebase](https://firebase.google.com)
     Livraria: [Discord](https://discord.js.org/#/)
     Ping: \`${bot.ws.ping}ms\`
-    VersÃ£o: \`1.0.0\``)
+    Versão: \`1.0.0\``)
     .addField('CPU & VPN', `CPU: \`${os.cpus().map((i) => `${i.model}`)[0]}\`
  <:imac:727314907829370891> Plataforma: \`${os.platform()}\`
 <:memoria:727314935960436916> Memoria Ram: \`${(os.totalmem() /1024 /1024).toFixed(2)} mb.\`
 <:memoria:727314935960436916> Memoria Restante: \`${(os.freemem() /1024 /1024).toFixed(2)} mb.\`
 <:memoria:727314935960436916> Memoria Usada: \`${(process.memoryUsage().heapUsed / 1024 / 1024 /2).toFixed(2)}%\``)
-    .addField("Ãšteis:", `**[Me Adicione](https://discord.com/oauth2/authorize?client_id=730788747490099270&scope=bot&permissions=0)**
-    **[Suporte](https://discord.gg/ANRpgVg)**`)
+    .addField("Úteis:", `**[Me Adicione](https://discord.com/oauth2/authorize?client_id=714937188059447377&scope=bot&permissions=8)**
+  **[Suporte](https://discord.gg/ANRpgVg)**`)
     message.channel.send(but)
     }
 
@@ -54,11 +54,11 @@ exports.run = async(bot, message, args) => {
         const but = new Discord.MessageEmbed()
         .setAuthor("About Me")
         .setDescription("I was born on 07/06/2020, amid the Covid-19 Pandemic! \ NMy intention is to bring entertainment and moderation to your server!")
-        .addField("My creator:", "Pedr1n â™ª#4537")
-        .addField("Statistics", `ðŸ‘¥ Members: \`${memberCount}\`
-        ðŸ’¾ Guilds: \`${bot.guilds.cache.size}\`
-        ðŸ“ Channels: \`${bot.channels.cache.size}\`
-        ðŸ“Œ Commands: \`${bot.commands.size}\``)
+        .addField("My creator:", "Éld3rr_#2583")
+        .addField("Statistics", `Members: \`${memberCount}\`
+        Guilds: \`${bot.guilds.cache.size}\`
+        Channels: \`${bot.channels.cache.size}\`
+        Commands: \`${bot.commands.size}\``)
         .addField("Informations", `Host: [Heroku](https://heroku.com)
         Database: [Firebase](https://firebase.google.com)
         Livrary: [Discord](https://discord.js.org/#/)
@@ -69,8 +69,8 @@ exports.run = async(bot, message, args) => {
     <:memoria:727314935960436916> Memory Ram: \`${(os.totalmem() /1024 /1024).toFixed(2)} mb.\`
     <:memoria:727314935960436916> Remaining memory: \`${(os.freemem() /1024 /1024).toFixed(2)} mb.\`
     <:memoria:727314935960436916> Memory Usad: \`${(process.memoryUsage().heapUsed / 1024 / 1024 /2).toFixed(2)}%\``)
-        .addField("Useful:", `**[Add Me](https://discord.com/oauth2/authorize?client_id=730788747490099270&scope=bot&permissions=0)**
-        **[Support](https://discord.gg/ANRpgVg)**`)
+        .addField("Useful:", `**[Add Me](https://discord.com/oauth2/authorize?client_id=714937188059447377&scope=bot&permissions=0)**
+      **[Support](https://discord.gg/ANRpgVg)**`)
         message.channel.send(but)
     }
 }
