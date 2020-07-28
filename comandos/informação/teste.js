d135458b3417a9f6c843e322aa5db12a13f3861d
Eld3rr <66041715+Eld3rr@users.noreply.github.com>
2020-07-28 17:50:09 -0300

    Create teste.js

---
+++ comandos/informação/teste.js
@@ -0,0 +1,24 @@
+const jimp = require("jimp")
+const Discord = require("discord.js")
+
+exports.run = async (client, message, args) => {
+let fonte = await jimp.loadFont(jimp.FONT_SANS_32_BLACK)
+let mask = await jimp.read('mascara.png')
+let avatar = await jimp.read(message.author.displayAvatarURL())
+
+
+jimp.read('https://sm.ign.com/ign_br/news/a/avatar-the/avatar-the-last-airbender-is-getting-expansion-novels_sma8.jpg').then(avatar => {
+    avatar.resize(130, 130)
+    mask.resize(130, 130)
+    avatar.mask(mask)
+    fundo.composite(avatar,40, 90).write('beta.png')
+  })
+  .catch(err => {
+    console.log('Erro ao carregar a imagem')
+  });
+
+})
+exports.help = {
+name: "teste",
+aliases: []
+}
